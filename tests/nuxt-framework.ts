1bb4f73ba53763cbd1c63afd27850082c267ee72
Haoqun Jiang <haoqunjiang@gmail.com>
2023-01-28 15:33:47 +0800

    fix: in our setup, monorepos don't need to manually specify overrides
    - pnpm respects `pnpm.overrides` in root `package.json`s.
    - we've already added all vue packages to the root overrides field

--- tests/nuxt-framework.ts
+++ tests/nuxt-framework.ts
@@ -5,13 +5,6 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/framework',
-		// https://github.com/vitejs/vite-ecosystem-ci/pull/158#discussion_r1038778127
-		// For monorepos, overrides need to be manually specified.
-		overrides: {
-			'@vue/reactivity': true,
-			'@vue/shared': true,
-			vue: true,
-		},
 		build: 'build',
 		test: ['test:fixtures', 'test:types', 'test:unit'],
 	})
