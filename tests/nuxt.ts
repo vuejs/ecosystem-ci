4eba8e82a020f330eba04f1b8805178349eacac3
Evan You <yyx990803@gmail.com>
2023-11-28 08:44:06 +0800

    nuxt edge case

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -1,12 +1,28 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
+import path from 'node:path'
+import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/nuxt',
 		build: 'build',
-		beforeTest: 'pnpm playwright-core install chromium',
+		beforeTest: [
+			'pnpm playwright-core install chromium',
+			async () => {
+				const dir = path.resolve(options.workspace, 'nuxt')
+				const filePath = path.resolve(
+					dir,
+					'packages/nuxt/test/auto-import.test.ts',
+				)
+				const file = fs.readFileSync(filePath, 'utf-8')
+				fs.writeFileSync(
+					filePath,
+					file.replace("'compile'", "'compile', 'ErrorTypeStrings'"),
+				)
+			},
+		],
 		test: ['test:fixtures', 'test:types', 'test:unit'],
 	})
 }
