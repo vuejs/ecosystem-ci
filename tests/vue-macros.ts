d83e417f70f854ffb9c1854380f0424696124761
Evan You <yyx990803@gmail.com>
2023-12-25 15:16:28 +0800

    vue macros

--- tests/vue-macros.ts
+++ tests/vue-macros.ts
@@ -1,5 +1,7 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
+import path from 'node:path'
+import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -7,6 +9,18 @@ export async function test(options: RunOptions) {
 		repo: 'vue-macros/vue-macros',
 		branch: 'vue-ecosystem-ci',
 		build: 'build',
+		async beforeTest() {
+			const dir = path.resolve(options.workspace, 'vue-macros')
+			const filePath = path.resolve(dir, 'tsconfig.base.json')
+			const file = fs.readFileSync(filePath, 'utf-8')
+			fs.writeFileSync(
+				filePath,
+				file.replace(
+					'"jsx": "preserve",',
+					'"jsx": "preserve",\n  "jsxImportSource": "vue",',
+				),
+			)
+		},
 		test: ['test:ecosystem'],
 	})
 }
