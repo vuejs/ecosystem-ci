ccb0256ddeadb0a85773b1bf5da39dd79d301f83
Evan You <yyx990803@gmail.com>
2023-12-25 16:26:25 +0800

    fix vuetify patch

--- tests/vuetify.ts
+++ tests/vuetify.ts
@@ -8,13 +8,13 @@ export async function test(options: RunOptions) {
 		...options,
 		repo: 'vuetifyjs/vuetify',
 		branch: 'master',
-		build: 'yarn workspace vuetify run build',
-		async beforeTest() {
+		beforeBuild: async () => {
 			const dir = path.resolve(options.workspace, 'vuetify')
 			const filePath = path.resolve(dir, 'packages/vuetify/src/globals.d.ts')
 			const file = fs.readFileSync(filePath, 'utf-8')
 			fs.writeFileSync(filePath, `import 'vue/jsx'\n` + file)
 		},
+		build: 'yarn workspace vuetify run build',
 		// there's also an e2e test script in vuetify,
 		// but it seems flaky, so I skipped it for now
 		test: ['yarn lerna run test:coverage -- -- -i'],
