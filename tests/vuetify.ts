7997a2b4705769fd55ed9688e5ae3d212d6dda9f
Evan You <yyx990803@gmail.com>
2023-12-25 15:21:36 +0800

    jsx for vuetify

--- tests/vuetify.ts
+++ tests/vuetify.ts
@@ -1,5 +1,7 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
+import path from 'node:path'
+import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -7,6 +9,18 @@ export async function test(options: RunOptions) {
 		repo: 'vuetifyjs/vuetify',
 		branch: 'master',
 		build: 'yarn workspace vuetify run build',
+		async beforeTest() {
+			const dir = path.resolve(options.workspace, 'vuetify')
+			const filePath = path.resolve(dir, 'tsconfig.base.json')
+			const file = fs.readFileSync(filePath, 'utf-8')
+			fs.writeFileSync(
+				filePath,
+				file.replace(
+					'"jsx": "preserve",',
+					'"jsx": "preserve",\n  "jsxImportSource": "vue",',
+				),
+			)
+		},
 		// there's also an e2e test script in vuetify,
 		// but it seems flaky, so I skipped it for now
 		test: ['yarn lerna run test:coverage -- -- -i'],
