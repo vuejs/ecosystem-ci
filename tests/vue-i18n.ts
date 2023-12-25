711cfa7f20ca8a4e2060c8ba49b80d676c8f3d52
Evan You <yyx990803@gmail.com>
2023-12-25 17:00:10 +0800

    remove no longer needed patch

--- tests/vue-i18n.ts
+++ tests/vue-i18n.ts
@@ -1,7 +1,5 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
-import path from 'node:path'
-import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -9,18 +7,7 @@ export async function test(options: RunOptions) {
 		repo: 'intlify/vue-i18n-next',
 		branch: 'master',
 		build: 'build --all -t',
-		beforeTest: [
-			'pnpm playwright install chromium',
-			async () => {
-				const dir = path.resolve(options.workspace, 'vue-i18n-next')
-				const filePath = path.resolve(
-					dir,
-					'test-dts/vue-i18n/components.test-d.tsx',
-				)
-				const file = fs.readFileSync(filePath, 'utf-8')
-				fs.writeFileSync(filePath, `import 'vue/jsx'\n` + file)
-			},
-		],
+		beforeTest: ['pnpm playwright install chromium'],
 		test: ['test:cover', 'test:type', 'test:e2e'],
 	})
 }
