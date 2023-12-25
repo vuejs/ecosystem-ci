a3123606778e2554f33c1dd94e0b1e34befab7cc
Evan You <yyx990803@gmail.com>
2023-12-25 12:54:27 +0800

    temp fix for vue-i18n

--- tests/vue-i18n.ts
+++ tests/vue-i18n.ts
@@ -1,5 +1,7 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
+import path from 'node:path'
+import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -7,7 +9,18 @@ export async function test(options: RunOptions) {
 		repo: 'intlify/vue-i18n-next',
 		branch: 'master',
 		build: 'build --all -t',
-		beforeTest: 'pnpm playwright install chromium',
+		beforeTest: [
+			'pnpm playwright install chromium',
+			async () => {
+				const dir = path.resolve(options.workspace, 'vue-i18n')
+				const filePath = path.resolve(
+					dir,
+					'test-dts/vue-i18n/components.test-d.tsx',
+				)
+				const file = fs.readFileSync(filePath, 'utf-8')
+				fs.writeFileSync(filePath, `import 'vue/jsx'\n` + file)
+			},
+		],
 		test: ['test:cover', 'test:type', 'test:e2e'],
 	})
 }
