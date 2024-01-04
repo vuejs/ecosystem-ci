16d8280112c743398bce072d1642b8f2b3622a04
Evan You <yyx990803@gmail.com>
2024-01-04 14:31:50 +0800

    temporary vuetify jsx fix

--- tests/vuetify.ts
+++ tests/vuetify.ts
@@ -1,11 +1,19 @@
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
+import path from 'node:path'
+import fs from 'node:fs'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vuetifyjs/vuetify',
 		branch: 'master',
+		beforeBuild: async () => {
+			const dir = path.resolve(options.workspace, 'vuetify')
+			const filePath = path.resolve(dir, 'packages/vuetify/src/globals.d.ts')
+			const file = fs.readFileSync(filePath, 'utf-8')
+			fs.writeFileSync(filePath, `import 'vue/jsx'\n` + file)
+		},
 		build: 'yarn workspace vuetify run build',
 		// there's also an e2e test script in vuetify,
 		// but it seems flaky, so I skipped it for now
