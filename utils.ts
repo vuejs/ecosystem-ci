8bebb635f8c5e7a5b9f840ba42cff8f1f25ec91f
Haoqun Jiang <haoqunjiang@gmail.com>
2024-03-19 23:45:53 +0800

    refactor: run patchFiles before any commands
    So that we have the chance to modify the `package.json` before the
    dependency installation.

--- utils.ts
+++ utils.ts
@@ -242,6 +242,17 @@ export async function runInRepo(options: RunOptions & RepoOptions) {
 			).join(', ')}`,
 		)
 	}
+
+	if (patchFiles) {
+		for (const fileName in patchFiles) {
+			const filePath = path.resolve(dir, fileName)
+			const patchFn = patchFiles[fileName]
+			const content = fs.readFileSync(filePath, 'utf-8')
+			fs.writeFileSync(filePath, patchFn(content))
+			console.log(`patched file: ${fileName}`)
+		}
+	}
+
 	const agent = options.agent
 	const beforeInstallCommand = toCommand(beforeInstall, agent)
 	const beforeBuildCommand = toCommand(beforeBuild, agent)
@@ -286,16 +297,6 @@ export async function runInRepo(options: RunOptions & RepoOptions) {
 		}
 	}
 	await applyPackageOverrides(dir, pkg, overrides)
-
-	if (patchFiles) {
-		for (const fileName in patchFiles) {
-			const filePath = path.resolve(dir, fileName)
-			const patchFn = patchFiles[fileName]
-			const content = fs.readFileSync(filePath, 'utf-8')
-			fs.writeFileSync(filePath, patchFn(content))
-			console.log(`patched file: ${fileName}`)
-		}
-	}
 
 	await beforeBuildCommand?.(pkg.scripts)
 	await buildCommand?.(pkg.scripts)
