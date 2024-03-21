a0721cbb95c3dd244e3b08b31cc636b78f02c7ee
Haoqun Jiang <haoqunjiang@gmail.com>
2024-03-21 11:29:09 +0800

    fix: dedupe source-map-js in vuetify to fix the build

--- tests/vuetify.ts
+++ tests/vuetify.ts
@@ -10,5 +10,16 @@ export async function test(options: RunOptions) {
 		// there's also an e2e test script in vuetify,
 		// but it seems flaky, so I skipped it for now
 		test: ['yarn lerna run test:coverage -- -- -i'],
+		patchFiles: {
+			'package.json': (content) => {
+				const pkg = JSON.parse(content)
+				// As of 2024-03-21, the version of source-map-js from vuetify isn't the same as the one from vue core.
+				// Therefore a "Duplicate identifier" TS error occurs when building.
+				// But we can't run dedupe in the vuetify repo, as the build would fail, too.
+				// So we need to overwrite the version to ^1.1.0 to avoid duplications of the dependency.
+				pkg.resolutions['source-map-js'] = '^1.1.0'
+				return JSON.stringify(pkg, null, 2)
+			},
+		},
 	})
 }
