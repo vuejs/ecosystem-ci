1a981015f52a9ff2eaeda92240b2d8f683b00f34
Haoqun Jiang <haoqunjiang@gmail.com>
2024-03-28 22:23:17 +0800

    fix: the workaround for quasar qselect tests is no longer needed
    See https://github.com/quasarframework/quasar-testing/issues/343#issuecomment-2004646647

--- tests/quasar.ts
+++ tests/quasar.ts
@@ -8,16 +8,5 @@ export async function test(options: RunOptions) {
 		branch: 'dev',
 		build: 'vue-ecosystem-ci:build',
 		test: 'vue-ecosystem-ci:test',
-		// Need to skip QSelect tests until https://github.com/quasarframework/quasar-testing/issues/343 is resolved
-		patchFiles: {
-			'ui/dev/cypress.config.cjs': (content) =>
-				content +
-				`
-					module.exports.component.excludeSpecPattern = [
-						'../src/components/**/QSelect.cy.js',
-						'../src/composables/**/use-validate.cy.js'
-					]
-					`,
-		},
 	})
 }
