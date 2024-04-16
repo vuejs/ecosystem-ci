42e9e4674ac94e296aa25d818f28e5399cbf852d
Haoqun Jiang <haoqunjiang@gmail.com>
2024-04-16 20:44:07 +0800

    chore: add primevue 3.51.0 test
    It's fixed to 3.51.0 because its unit tests often break.
    I'll update the version field manually every few weeks.

---
+++ tests/primevue.ts
@@ -0,0 +1,20 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'primefaces/primevue',
+		tag: '3.51.0',
+		build: 'build',
+		test: 'test:unit',
+		patchFiles: {
+			'components/lib/inputnumber/InputNumber.spec.js': (content) => {
+				return content.replaceAll(
+					`it('is keypress called`,
+					`it.skip('is keypress called`,
+				)
+			},
+		},
+	})
+}
