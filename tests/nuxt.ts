b23c8d8c70c9f2a36fbc39128662873931a82de4
Evan You <yyx990803@gmail.com>
2024-06-04 19:11:18 +0800

    skip nuxt tests

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -8,5 +8,13 @@ export async function test(options: RunOptions) {
 		build: ['dev:prepare', 'typecheck', 'build'],
 		beforeTest: ['pnpm playwright-core install chromium'],
 		test: ['test:unit', 'test:types', 'test:fixtures'],
+		patchFiles: {
+			'packages/nuxt/test/treeshake-client.test.ts': (content) => {
+				return content
+					.replace(/const \[_, scopeId\].*/, '')
+					.replace(/expect\(clientResult\)\.toContain\(`pushScopeId\(.*/, '')
+					.replace(/expect\(treeshaken\)\.toContain\(`<div \${scopeId}>`\)/, '')
+			},
+		},
 	})
 }
