72fc34bfa3a45c8b6fba364b8cdaa495ad21f656
Evan You <yyx990803@gmail.com>
2024-06-04 18:11:17 +0800

    fix missing comma

--- tests/vitepress.ts
+++ tests/vitepress.ts
@@ -11,7 +11,7 @@ export async function test(options: RunOptions) {
 		patchFiles: {
 			'package.json': (content) => {
 				// temporary bug in vue-tsc 2.0.14+ for 3.5 types
-				return content.replace(/"vue-tsc": .*/, '"vue-tsc": "2.0.14"')
+				return content.replace(/"vue-tsc": .*/, '"vue-tsc": "2.0.14",')
 			},
 		},
 	})
