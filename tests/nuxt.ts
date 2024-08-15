8b391efadb6f9b503ee0208d0f789643052b55f8
Evan You <evan@vuejs.org>
2024-08-15 14:39:19 +0800

    nuxt: add new imported methods

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -1,6 +1,17 @@
 import { runInRepo } from '../utils.ts'
 import { RunOptions } from '../types.ts'
 
+const newMethods = [
+	'useTemplateRef',
+	'useId',
+	'useHost',
+	'useShadowRoot',
+	'hydrateOnVisible',
+	'hydrateOnMediaQuery',
+	'hydrateOnInteraction',
+	'hydrateOnIdle',
+]
+
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
@@ -14,6 +25,12 @@ export async function test(options: RunOptions) {
 					.replace(/const \[_, scopeId\].*/, '')
 					.replace(/expect\(clientResult\)\.toContain\(`pushScopeId\(.*/, '')
 					.replace(/expect\(treeshaken\)\.toContain\(`<div \${scopeId}>`\)/, '')
+			},
+			'packages/nuxt/src/imports/presets.ts': (content) => {
+				return content.replace(
+					`'useTransitionState',`,
+					`'useTransitionState', ${newMethods.map((m) => `'${m}'`).join(',')}`,
+				)
 			},
 		},
 	})
