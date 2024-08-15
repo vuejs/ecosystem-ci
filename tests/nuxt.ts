cf4d55218c07d9d1d65176c8da09ac0e1f7d0bf3
Evan You <evan@vuejs.org>
2024-08-15 15:20:40 +0800

    nuxt: use 3.5 branch

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -1,17 +1,6 @@
 import { runInRepo } from '../utils.ts'
 import { RunOptions } from '../types.ts'
 
-const newMethods = [
-	'useTemplateRef',
-	'useId',
-	'useHost',
-	'useShadowRoot',
-	'hydrateOnVisible',
-	'hydrateOnMediaQuery',
-	'hydrateOnInteraction',
-	'hydrateOnIdle',
-]
-
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
@@ -19,19 +8,6 @@ export async function test(options: RunOptions) {
 		build: ['dev:prepare', 'typecheck', 'build'],
 		beforeTest: ['pnpm playwright-core install chromium'],
 		test: ['test:unit', 'test:types', 'test:fixtures'],
-		patchFiles: {
-			'packages/nuxt/test/treeshake-client.test.ts': (content) => {
-				return content
-					.replace(/const \[_, scopeId\].*/, '')
-					.replace(/expect\(clientResult\)\.toContain\(`pushScopeId\(.*/, '')
-					.replace(/expect\(treeshaken\)\.toContain\(`<div \${scopeId}>`\)/, '')
-			},
-			'packages/nuxt/src/imports/presets.ts': (content) => {
-				return content.replace(
-					`'useTransitionState',`,
-					`'useTransitionState', ${newMethods.map((m) => `'${m}'`).join(',')}`,
-				)
-			},
-		},
+		branch: 'chore/vue-3.5',
 	})
 }
