eaa58079915e11798fd315afda9c79d275802d24
Evan You <evan@vuejs.org>
2024-08-15 14:58:40 +0800

    use ecosystem-ci branch for vue-macros

--- tests/vue-macros.ts
+++ tests/vue-macros.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'sxzz/unplugin-vue-macros',
-		branch: 'main',
+		branch: 'vue-ecosystem-ci',
 		build: 'build',
 		test: ['test:ecosystem'],
 	})
