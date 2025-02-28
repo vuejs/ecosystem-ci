2999b915511fe7424ebec510c09826a3a7eb57cf
Haoqun Jiang <haoqunjiang@gmail.com>
2025-02-28 16:24:35 +0800

    chore: update primevue to latest

--- tests/primevue.ts
+++ tests/primevue.ts
@@ -5,16 +5,10 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'primefaces/primevue',
-		tag: '3.53.0',
-		build: 'build',
+		branch: 'master',
+		// 4.3.1 but not tagged on GitHub. It's the latest commit that with passing tests.
+		commit: 'e53d48ed62dc845d8f8ca3c7ccd00941229b98f9',
+		build: ['format', 'build'],
 		test: 'test:unit',
-		patchFiles: {
-			'components/lib/inputnumber/InputNumber.spec.js': (content) => {
-				return content.replaceAll(
-					`it('is keypress called`,
-					`it.skip('is keypress called`,
-				)
-			},
-		},
 	})
 }
