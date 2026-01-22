775f692d33ab80f093235003ca2c4269b5105a3b
daiwei <daiwei521@126.com>
2026-01-22 14:58:08 +0800

    fix(vueuse): add patchFiles to disable trustPolicy in pnpm-workspace.yaml

--- tests/vueuse.ts
+++ tests/vueuse.ts
@@ -1,12 +1,24 @@
 import { runInRepo } from '../utils.ts'
 import { RunOptions } from '../types.ts'
 import { REGISTRY_ADDRESS } from '../registry.ts'
+import YAML from 'yaml'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vueuse/vueuse',
 		branch: 'main',
+		patchFiles: {
+			'pnpm-workspace.yaml': (content) => {
+				try {
+					const data = YAML.parse(content) ?? {}
+					data.trustPolicy = 'off'
+					return YAML.stringify(data)
+				} catch {
+					return content
+				}
+			},
+		},
 		beforeBuild: `pnpm dedupe --registry=${REGISTRY_ADDRESS}`,
 		build: 'build',
 		test: ['typecheck', 'test'],
