c9a676399c7d2cab650b177ce14d6e00ed3abdb5
daiwei <daiwei521@126.com>
2026-05-27 15:57:27 +0800

    fix: add patchFiles to nuxt and quasar test configurations

--- tests/quasar.ts
+++ tests/quasar.ts
@@ -6,6 +6,14 @@ export async function test(options: RunOptions) {
 		...options,
 		repo: 'quasarframework/quasar',
 		branch: 'dev',
+		patchFiles: {
+			'ui/package.json': (content) => {
+				const pkg = JSON.parse(content)
+				pkg.devDependencies ||= {}
+				pkg.devDependencies['@vue/test-utils'] ||= '^2.4.10'
+				return `${JSON.stringify(pkg, null, 2)}\n`
+			},
+		},
 		build: 'vue-ecosystem-ci:build',
 		test: 'vue-ecosystem-ci:test',
 	})
