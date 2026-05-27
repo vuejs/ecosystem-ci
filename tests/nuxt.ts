0230f4baf08f2713a97a9a1fbfcb83a0778108c5
daiwei <daiwei521@126.com>
2026-05-27 15:57:27 +0800

    fix: add patchFiles to nuxt and quasar test configurations

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -1,11 +1,19 @@
 import { runInRepo } from '../utils.ts'
 import { RunOptions } from '../types.ts'
+import YAML from 'yaml'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/nuxt',
 		branch: 'main',
+		patchFiles: {
+			'pnpm-workspace.yaml': (content) => {
+				const data = YAML.parse(content) ?? {}
+				data.blockExoticSubdeps = false
+				return YAML.stringify(data)
+			},
+		},
 		build: ['dev:prepare', 'typecheck', 'build'],
 		beforeTest: ['pnpm playwright-core install chromium'],
 		test: ['test:unit', 'test:types', 'test:fixtures'],
